83dd4a047b70cb10308d696973fbe3c74fa73f0a
Charles Obuzor <charlesobuzor@outlook.com>
2023-03-11 22:31:03 +0100

    + responsiveness

--- src/List.js
+++ src/List.js
@@ -4,6 +4,7 @@ const List = ({data, setData, datafile}) => {
    const filterRole = (tag) => {
       const $new = data.filter(newData => newData.role == tag)
       setData($new)
+      setFilterList('1')
    }
    const filterLevel = (tag) => {
       const $new = data.filter(newData => newData.level == tag)
@@ -26,35 +27,37 @@ const List = ({data, setData, datafile}) => {
 
          {data.map((item) => (
             <div className={`list-item ${item.featured && 'sidebar'}`} key={item.id}>
-               <img src={item.logo} alt=""/>
-               
-               <div className="item-section">
-                  <div>
-                     <p className="item-company">{item.company}</p>
-                     {item.new && <span className="item-new">New!</span>}
-                     {item.featured && <span className="item-featured">Featured</span>}
-                  </div>
+               <img src={item.logo} draggable='false'/>
+            
+               <div>
+                  <div className="item-section">
+                     <div>
+                        <p className="item-company">{item.company}</p>
+                        {item.new && <span className="item-new">New!</span>}
+                        {item.featured && <span className="item-featured">Featured</span>}
+                     </div>
 
-                  <p className="item-position">{item.position}</p>
-                  <p className="item-info">
-                     <span>{item.postedAt}</span> 
-                     <span>•</span> 
-                     <span>{item.contract}</span>
-                     <span>•</span> 
-                     <span>{item.location}</span>
-                  </p>   
+                     <p className="item-position">{item.position}</p>
+                     <p className="item-info">
+                        <span>{item.postedAt}</span> 
+                        <span>•</span> 
+                        <span>{item.contract}</span>
+                        <span>•</span> 
+                        <span>{item.location}</span>
+                     </p>   
+                  </div>
+                     <ul className="item-tags">
+                        <li onClick={() => {filterRole(item.role)}}>{item.role}</li>
+                        <li onClick={() => {filterLevel(item.level)}}>{item.level}</li>
+                        {item.tools.map((tool, index) => (
+                           <li key={index} onClick={() => {filterTools(tool)}}>{tool}</li>
+                        ))} 
+                        {item.languages.map((lang, index) => (
+                           <li key={index} onClick={() => {filterLang(lang)}}>{lang}</li>
+                        ))}
+                     </ul>
                </div>
 
-               <ul className="item-tags">
-                  <li onClick={() => {filterRole(item.role)}}>{item.role}</li>
-                  <li onClick={() => {filterLevel(item.level)}}>{item.level}</li>
-                  {item.tools.map((tool, index) => (
-                     <li key={index} onClick={() => {filterTools(tool)}}>{tool}</li>
-                  ))} 
-                  {item.languages.map((lang, index) => (
-                     <li key={index} onClick={() => {filterLang(lang)}}>{lang}</li>
-                  ))}
-               </ul>
             </div>
          ))}
       </>
